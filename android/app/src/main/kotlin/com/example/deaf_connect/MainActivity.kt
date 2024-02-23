9bb97b0fedd57e5858019f6cbd9fc12a91090175
jadantoun0 <jadantoun77@gmail.com>
2024-02-23 19:43:14 +0200

    v1

---
+++ android/app/src/main/kotlin/com/example/deaf_connect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.deaf_connect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/database/db_helper.dart
@@ -0,0 +1,62 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DbHelper {
+  static Database? _database;
+
+  static Future<Database> getDb() async {
+    // if database is already created, return it
+    if (_database != null) {
+      return _database!;
+    }
+    // else, we create the db and return it
+    _database = await _createDB();
+    return _database!;
+  }
+
+  static Future<Database> _createDB() async {
+    final dbPath = await getDatabasesPath();
+    const dbName = 'deafconnect.db';
+
+    var database = await openDatabase(
+      join(dbPath, dbName),
+      version: 4,
+      onCreate: (Database database, int version) async {
+        await _createTables(database);
+      },
+    );
+    return database;
+  }
+
+  static Future _createTables(Database database) async {
+    // CREATING TRANSCRIPTS TABLE
+    await database.execute(""" 
+      CREATE TABLE IF NOT EXISTS transcripts (
+        transcript_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        transcript_name VARCHAR(255) NOT NULL,
+        date_created STRING
+      );
+    """);
+
+    // CREATING MESSAGES TABLE
+    await database.execute(""" 
+      CREATE TABLE IF NOT EXISTS messages (
+        message_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        transcript_id INT,
+        message_content TEXT,
+        is_received BOOLEAN,
+        message_date STRING,
+        ms_since_epoch INTEGER,
+        FOREIGN KEY (transcript_id) REFERENCES transcripts(transcript_id)
+      );
+    """);
+
+    // CREATING SHORTCUTS TABLE
+    await database.execute(""" 
+    CREATE TABLE IF NOT EXISTS shortcuts (
+      shortcut_order INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      shortcut_name VARCHAR(255)
+    );  
+    """);
+  }
+}
